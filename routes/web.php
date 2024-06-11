c65970a59542b0bff52cd785d14b6a7c8c801f91
Pedro <pedroforjobs@outlook.com>
2024-06-11 13:35:36 -0300

    controller e métodos criadosD'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +16,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Rotas de alunos
+
+Route::controller(StudentController::class)->group(function () {
+    
+    Route::get('/aluno/cadastrar', 'create')->name('student.create');
+
+    Route::post('/aluno/cadastrar', 'store')->name('student.store');
+
+    Route::get('/alunos/{status}/listar', 'index')->name('student.index');
+
+    Route::get('/aluno/editar/{id}', 'edit')->name('student.edit');
+
+    Route::put('/aluno/editar/{id}', 'update')->name('student.update');
+
+    Route::delete('/aluno/excluir/{id}', 'destroy')->name('student.destroy');
+
+    Route::get('/aluno/{id}/detalhes', 'show')->name('student.show');
+
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
