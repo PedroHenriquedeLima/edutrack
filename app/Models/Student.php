3a466bc09aba2954d35fadfc180ff59d0840275e
Pedro <pedroforjobs@outlook.com>
2024-06-11 13:23:02 -0300

    models, tabelas e relacionamentos criados

---
+++ app/Models/Student.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillabe = [
+        'name',
+        'contact',
+        'schedule_days',
+        'schedule_time',
+        'payment_date',
+        'user_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
